7117d8e8e665519587218bcce25ea36439abb1d6
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-24 00:42:52 -0500

    feat(project): create project and first component called Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.sass";
+
+const Header = () => {
+
+  return (
+    <article className="header">
+      <div className="header__title">
+        <p className="header__title--display">Ravn Start Wars Registry</p>
+      </div>
+    </article>
+  );
+};
+
+export default Header;
