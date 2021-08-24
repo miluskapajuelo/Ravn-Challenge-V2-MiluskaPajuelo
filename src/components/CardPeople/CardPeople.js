a8100a0bdc3890f3618331f6b8027da2a34f8ac9
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-24 03:01:28 -0500

    feat(component): create detail of people component

--- src/components/CardPeople/CardPeople.js
+++ src/components/CardPeople/CardPeople.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./CardPeople.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const CardPeople = () => {
-
   return (
     <article className="card">
-      <h2>Nombre comercial</h2>
-      <p>Nombre completo</p>
+      <div className="card__detail">
+        <div>
+          <h2>Nombre comercial</h2>
+          <p>Nombre completo</p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
+      </div>
     </article>
   );
 };
--- src/components/DetailPeople/DetailPeople.js
+++ src/components/DetailPeople/DetailPeople.js
@@ -4,8 +4,17 @@ import "./DetailPeople.sass";
 const DetailPeople = () => {
 
   return (
-    <article>
-      <p>holi DetailPeople</p>
+    <article className="detail">
+      <h2>General Information</h2>
+      <span >Eye Color<a>Blue</a></span>
+      <span >Hair Color<a>Blond</a></span>
+      <span >Skin Color<a>Fair</a></span>
+      <span >Birth Year<a>19BBY</a></span>
+      <br/>
+      <br/>
+      <h2>Vehicles</h2>
+      <span >Snowspeeder</span>
+      <span >Imperial Speeder Bike</span>
     </article>
   );
 };
