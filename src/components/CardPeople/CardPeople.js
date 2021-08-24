7ee86338a8abebf75f833b67bdfc00c5c085dd48
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-24 02:18:17 -0500

    feat(component): create card of people component

---
+++ src/components/CardPeople/CardPeople.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CardPeople.sass";
+
+const CardPeople = () => {
+
+  return (
+    <article className="card">
+      <h2>Nombre comercial</h2>
+      <p>Nombre completo</p>
+    </article>
+  );
+};
+
+export default CardPeople;
