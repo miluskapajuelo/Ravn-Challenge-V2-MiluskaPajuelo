68d71871f61c4b460774d90991210721bdf72076
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-24 19:25:04 -0500

    feat(component): add info from graphQL in component and media queries in styles file

--- src/components/PeopleStartWar/PeopleStartWar.js
+++ src/components/PeopleStartWar/PeopleStartWar.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
 import Header from "./../Header/Header";
 import CardPeople from "./../CardPeople/CardPeople";
 import DetailPeople from "./../DetailPeople/DetailPeople";
-import './PeopleStartWar.sass'
+import "./PeopleStartWar.sass";
+import { useQuery } from "@apollo/client";
+import {TRACKS} from '../../module/query'
+
+
 
 const PeopleStartWar = () => {
+  const {loading, error, data}= useQuery(TRACKS);
+  if(loading) return 'Loading';
+  if(error) return `error! ${error.message}`;
+  const dataPeople = data.allPeople.people
 
-   
-
-    return (
-      <main>
-        <Header/> 
-        <section className="people">
-        <aside className="people__card"> 
-        <CardPeople/>
-        <CardPeople/>
-        <CardPeople/>
-        <CardPeople/>
+  return (
+    <main>
+      <Header />
+      <section className="people">
+        <aside className="people__card">
+        {dataPeople.map(people =>  <CardPeople key={people.id} people={people} />)}       
         </aside>
         <div className="people__detail">
-        <DetailPeople/>
+          <DetailPeople />
         </div>
-        </section>
-      </main>
-    )
-}
+      </section>
+    </main>
+  );
+};
 
-export default PeopleStartWar
+export default PeopleStartWar;
