33bb19bf34107d9e1f64a5cc1ef1f367a210a414
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-24 19:46:45 -0500

    docs(packages): install packages and create some mixin

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.sass";
 import PeopleStartWar from "./components/PeopleStartWar/PeopleStartWar";
+import DetailPeople from "./components/DetailPeople/DetailPeople"
 
 function App() {
   return (
     <div className="app-container">
       <Router>
         <Switch>
-          <Route path="/peopleStartWar" component={PeopleStartWar} />
+          <Route path="/" component={PeopleStartWar} />
+          <Route path='/people/:id' component={DetailPeople} />
         </Switch>
       </Router>
     </div>
