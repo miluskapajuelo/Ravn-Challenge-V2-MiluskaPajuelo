e858143fcf051a0fdf995819a36e5bfe97f4b901
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-25 12:19:43 -0500

    feat(component): create component person to put individual info

---
+++ src/components/CardPerson/CardPerson.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./CardPerson.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { useHistory} from "react-router-dom"
+
+
+const CardPerson = ({ people }) => {
+
+  const history = useHistory();
+  
+  const detailPeople = () => {
+    history.push({ pathname: '/people', search:`${people.id}`, state: {id:people.id} })
+  };
+  
+  return (
+    <article className="card">
+      <div className="card__detail">
+        <div>
+          <h2>{people.name}</h2>
+          <p>
+            {people.species !== null ? people.species.name : "Human"} from{" "}
+            {people.homeworld.name}
+          </p>
+        </div>
+        <div onClick={detailPeople}>
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default CardPerson;
