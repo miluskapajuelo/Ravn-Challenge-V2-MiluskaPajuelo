221d04ab29c0edcfd7c7cef28b19ea14ba894378
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-25 12:22:54 -0500

    feat(component): modularize components

--- src/components/PeopleStartWar/PeopleStartWar.js
+++ src/components/PeopleStartWar/PeopleStartWar.js
@@ -1,25 +1,19 @@
 import React from "react";
-import Header from "./../Header/Header";
-import CardPeople from "./../CardPeople/CardPeople";
 import DetailPeople from "./../DetailPeople/DetailPeople";
+import CardPeople from "./../CardPeople/CardPeople";
 import "./PeopleStartWar.sass";
-import { useQuery } from "@apollo/client";
-import {TRACKS} from '../../module/query'
+
 
 
 
 const PeopleStartWar = () => {
-  const {loading, error, data}= useQuery(TRACKS);
-  if(loading) return 'Loading';
-  if(error) return `error! ${error.message}`;
-  const dataPeople = data.allPeople.people
+
 
   return (
     <main>
-      <Header />
       <section className="people">
         <aside className="people__card">
-        {dataPeople.map(people =>  <CardPeople key={people.id} people={people} />)}       
+          <CardPeople/>
         </aside>
         <div className="people__detail">
           <DetailPeople />
