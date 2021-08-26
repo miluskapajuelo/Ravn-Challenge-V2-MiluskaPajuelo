324e93b54a95266759bb942d5bdaaaa081e7b865
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-08-25 23:10:38 -0500

    feat(component):Ã add validations in header and integrate with responsive app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.sass";
 import PeopleStartWar from "./components/PeopleStartWar/PeopleStartWar";
 import DetailPeople from "./components/DetailPeople/DetailPeople"
-import Header from "./components/Header/Header"
+
 
 function App() {
   return (
     
     <div className="app-container">
-      <Header />
       <Router>
         <Switch>
           <Route path="/" component={PeopleStartWar} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Header.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { useHistory} from "react-router-dom"
 
-const Header = () => {
-
+const Header = ({isVisible, setIsVisible}) => {
+  const history = useHistory();
+  const backToHome =()=>{
+    setIsVisible(false)
+    history.push('/')
+    
+  }
   return (
     <article className="header">
       <div className="header__title">
-        <p className="header__title--display">Ravn Start Wars Registry</p>
+        {isVisible?<p className="header__title inactive">DARK STARKWAYJER</p>:<p className="header__title inactive">People of Star Wars</p>}
+        
+        <div className={isVisible===true?'header__icon--active' :'header__icon--inactive'}><FontAwesomeIcon icon={faCoffee} onClick={backToHome}></FontAwesomeIcon></div>
       </div>
     </article>
   );
